1339c34c97681e251e6d36a73f37f22cd3d83283
Kian Stack <stackkian@gmail.com>
2014-10-23 21:51:42 -0500

    This is my older version of the code. I needed to stash it to get the newest

--- src/com/example/audiocapture/MainActivity.java
+++ src/com/example/audiocapture/MainActivity.java
@@ -13,11 +13,13 @@ import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -32,6 +34,10 @@ public class MainActivity extends Activity implements View.OnClickListener{
 	private ArrayList<Integer> FlagRelTimes;
 	private int flagCount = 0;
 	MediaPlayer m;
+	boolean isRecording;
+	Chronometer myChronometer;
+
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -43,12 +49,12 @@ public class MainActivity extends Activity implements View.OnClickListener{
 		start = (Button)findViewById(R.id.button1);
 		stop = (Button)findViewById(R.id.button2);
 		flag = (Button)findViewById(R.id.flagButton);
-
 		stop.setEnabled(false);
+		stop.setVisibility(View.INVISIBLE);
 		flag.setEnabled(false);
 		outputFile = Environment.getExternalStorageDirectory().
 				getAbsolutePath() + "/myrecording.3gp";
-
+		isRecording=false;
 		myAudioRecorder = new MediaRecorder();
 		myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
 		myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
@@ -57,6 +63,7 @@ public class MainActivity extends Activity implements View.OnClickListener{
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
 	}
+	
 	public void goToPlayback(View view){
 		Intent intent = new Intent(this, Playback.class);
 		intent.putExtra("fileName", Environment.getExternalStorageDirectory().
@@ -64,20 +71,93 @@ public class MainActivity extends Activity implements View.OnClickListener{
 		intent.putExtra("myArray", FlagRelTimes);
 		startActivity(intent);
 	}
-	public void start(View view){
-		try {
-			if(myAudioRecorder == null){
-				myAudioRecorder = new MediaRecorder();
-				myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-				myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-				myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
-				myAudioRecorder.setOutputFile(outputFile);
+	
+	
+	public void start(View view) throws IllegalArgumentException,
+	SecurityException, IllegalStateException, IOException{
+		myChronometer = (Chronometer)findViewById(R.id.chronometer);
+
+		try { 
+			if(isRecording==false)
+			{
+				isRecording=true;
+			    start.setText("Stop");
+			    
+			    if(myAudioRecorder == null){
+					myAudioRecorder = new MediaRecorder();
+					myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+					myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+					myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+					myAudioRecorder.setOutputFile(outputFile);
+				}
+	            myChronometer.setBase(SystemClock.elapsedRealtime());
+			    myChronometer.start();
+				myAudioRecorder.prepare();
+				myAudioRecorder.start();
+				//Grab Start Time
+				StartTime = System.currentTimeMillis();
+				Log.v("Time Elements", Long.toString(StartTime));
+				
+				//start.setEnabled(false);
+				//stop.setEnabled(true);
+				flag.setEnabled(true);
+				Toast.makeText(getApplicationContext(), "Recording started", Toast.LENGTH_LONG).show();
+			    
 			}
-			myAudioRecorder.prepare();
-			myAudioRecorder.start();
-			//Grab Start Time
-			StartTime = System.currentTimeMillis();
-			Log.v("Time Elements", Long.toString(StartTime));
+			else if(isRecording==true)
+			{
+				AlertDialog.Builder alertDialog  = new AlertDialog.Builder(MainActivity.this);
+				alertDialog.setTitle("Save File");
+				alertDialog.setMessage("Are you sure you want to save this recording?");
+				alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int which) {
+					}
+				});
+				alertDialog.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int which) {
+						myChronometer.stop();
+						myAudioRecorder.stop();
+						//Grab End Time
+						EndTime = System.currentTimeMillis();
+						//      flagTimes.add(EndTime);
+						TotalTime = (EndTime - StartTime)/1000;
+						Log.v("Time Elements", Long.toString(EndTime));
+						Log.v("Time Elements", Long.toString(TotalTime));
+						myAudioRecorder.release();
+						myAudioRecorder  = null;
+						stop.setEnabled(false);
+						flag.setEnabled(false);
+						start.setEnabled(true);
+						//		play.setEnabled(true);
+						Toast.makeText(getApplicationContext(), "Audio recorded successfully",
+								Toast.LENGTH_LONG).show();
+						//m.setDataSource(outputFile);
+						
+						AlertDialog.Builder alertDialog  = new AlertDialog.Builder(MainActivity.this);
+
+						alertDialog.setTitle("Edit File Name");
+						alertDialog.setMessage("What is the name of this recording?");
+						final EditText input = new EditText(MainActivity.this);
+						alertDialog.setView(input);
+						alertDialog.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int which) {
+								
+								Context context = getApplicationContext();
+								String fileName = input.getText().toString();
+								CharSequence text = "File '"+ fileName +"' has saved!";
+								int duration = Toast.LENGTH_SHORT;
+
+								Toast toast = Toast.makeText(context, text, duration);
+								toast.show();
+							}
+						});
+						alertDialog.show();
+					}
+				});
+				alertDialog.show();
+			}
+			
+			
 		} catch (IllegalStateException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -86,10 +166,7 @@ public class MainActivity extends Activity implements View.OnClickListener{
 			e.printStackTrace();
 		}
 
-		start.setEnabled(false);
-		stop.setEnabled(true);
-		flag.setEnabled(true);
-		Toast.makeText(getApplicationContext(), "Recording started", Toast.LENGTH_LONG).show();
+		
 
 	}
 	public void stop(View view) throws IllegalArgumentException,
@@ -104,7 +181,7 @@ public class MainActivity extends Activity implements View.OnClickListener{
 		});
 		alertDialog.setPositiveButton("Save", new DialogInterface.OnClickListener() {
 			public void onClick(DialogInterface dialog, int which) {
-				
+				//myChronometer.stop();
 				myAudioRecorder.stop();
 				//Grab End Time
 				EndTime = System.currentTimeMillis();
@@ -149,16 +226,16 @@ public class MainActivity extends Activity implements View.OnClickListener{
 
 
 	public void flag(View view){
-		flagCount++;
-		if(flagCount <= 3){
+//		flagCount++;
+//		if(flagCount <= 3){
 			int relativeTime;
 			FlagAbTimes.add(System.currentTimeMillis());
 			relativeTime = (int)(FlagAbTimes.get(FlagAbTimes.size()-1) - StartTime);
 			FlagRelTimes.add(relativeTime);
-			if(flagCount == 3){
-				flag.setEnabled(false);
-			}
-		}
+//			if(flagCount == 3){
+//				flag.setEnabled(false);
+//			}
+//		}
 	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
--- src/com/example/audiocapture/Playback.java
+++ src/com/example/audiocapture/Playback.java
@@ -2,33 +2,210 @@ package com.example.audiocapture;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.format.DateUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
+import android.widget.ScrollView;
 import android.widget.Toast;
 
 public class Playback extends Activity {
 	String fileName;
 	private ArrayList<Integer> FlagRelTimes;
 	MediaPlayer m;
-	private Button play;
+	//private Button play;
+	int mySize;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		m = new MediaPlayer();
-		setContentView(R.layout.activity_playback);
+		//setContentView(R.layout.activity_playback);
 		Bundle b = new Bundle();
 		b=getIntent().getExtras();
-		play = (Button)findViewById(R.id.button3);
+		//play = (Button)findViewById(R.id.button3);
 		fileName = b.getString("fileName");
 		FlagRelTimes = b.getIntegerArrayList("myArray");
-		Log.v("Time Elements", fileName);
+		loadActivity();
+		
+	}
+		
+	void loadActivity()
+	{
+		mySize = FlagRelTimes.size();
+		LinearLayout baseLayout = new LinearLayout(this);
+		baseLayout.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+		baseLayout.setOrientation(LinearLayout.VERTICAL);
+		baseLayout.setBackgroundColor(Color.parseColor("#000000"));
+		
+		ScrollView sv = new ScrollView(this);
+        sv.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.MATCH_PARENT));
+		final LinearLayout linLayout = new LinearLayout(this);
+        // specifying vertical orientation
+        linLayout.setOrientation(LinearLayout.VERTICAL);
+        // creating LayoutParams  
+        LayoutParams linLayoutParam = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT); 
+        // set LinearLayout as a root element of the screen 
+        //setContentView(linLayout, linLayoutParam);
+        setContentView(baseLayout);
+        Button play = new Button(this);
+        Button flag = new Button(this);
+		linLayout.setBackgroundColor(Color.parseColor("#50ADD8E6"));
+		//linLayout.setAlpha(new Float(.5));
+        LinearLayout horizLayout = new LinearLayout(this);
+        horizLayout.setOrientation(LinearLayout.HORIZONTAL);      
+        horizLayout.setPadding(5, 5, 5, 5);
+        flag.setText("flag");
+        play.setText("Play");
+        horizLayout.addView(flag);
+        horizLayout.addView(play);
+        baseLayout.addView(horizLayout);
+       
+        baseLayout.addView(sv);
+        sv.addView(linLayout);
+        LayoutParams lpView = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        
+        
+//        linLayout.addView(play, lpView);
+//        linLayout.addView(flag, lpView);
+        flag.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				FlagRelTimes.add(m.getCurrentPosition());
+				loadActivity();
+			}
+		});
+        play.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				m.reset();
+				if(m.isPlaying()){
+					m.stop();	
+				}else{
+					try {
+						m.setDataSource(fileName);
+					} catch (IllegalArgumentException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (SecurityException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IllegalStateException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				try {
+					m.prepare();
+				} catch (IllegalStateException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				m.start();
+				m.seekTo(0);
+				Toast.makeText(getApplicationContext(), "Playing audio from beginning", 
+						Toast.LENGTH_LONG).show();
+
+			}
+				
+			
+		});
+        Collections.sort(FlagRelTimes);
+        for( int i=0; i<FlagRelTimes.size(); i++)
+        {
+        	final LinearLayout horLayout = new LinearLayout(this);
+            horLayout.setOrientation(LinearLayout.HORIZONTAL);
+        	Button btn = new Button(this);
+        	Button delete = new Button(this);
+        	delete.setId(FlagRelTimes.get(i)+1);
+        	final int deleteid_ = delete.getId();
+        	delete.setText("Delete");
+            btn.setId(FlagRelTimes.get(i));
+            final int id_ = btn.getId();
+            btn.setText("Flag " + DateUtils.formatElapsedTime(id_/1000));
+            horLayout.addView(btn);
+            horLayout.addView(delete);
+            linLayout.addView(horLayout, lpView);
+            delete = ((Button)findViewById(deleteid_));
+    		
+            delete.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					AlertDialog.Builder alertDialog  = new AlertDialog.Builder(Playback.this);
+		    		alertDialog.setTitle("Delete Flag?");
+					alertDialog.setMessage("Are you sure you want to delete this flag?");
+		    		alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+		    			public void onClick(DialogInterface dialog, int which) {
+		    			}
+		    		});
+		    		alertDialog.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+		    			public void onClick(DialogInterface dialog, int which) {
+					linLayout.removeView(horLayout);
+					FlagRelTimes.remove(Integer.valueOf(deleteid_-1));
+		    			
+		    			}
+		    			});
+					alertDialog.show();
+				}
+			});
+		    btn = ((Button) findViewById(id_));
+            btn.setOnClickListener(new View.OnClickListener() {
+		        public void onClick(View view) {  	
+		        	m.reset();
+		    		if(m.isPlaying()){
+		    			m.stop();	
+		    		}else{
+		    			try {
+							m.setDataSource(fileName);
+						} catch (IllegalArgumentException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (SecurityException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (IllegalStateException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+		    		}
+		    		try {
+						m.prepare();
+					} catch (IllegalStateException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+		    		m.start();
+		    		m.seekTo(id_);
+		    		Toast.makeText(getApplicationContext(), "Playing audio from flag ", 
+		    				Toast.LENGTH_LONG).show();
+		        	
+		        }
+		    });
+        }		
 	}
 
 	@Override
@@ -49,65 +226,10 @@ public class Playback extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	public void play(View view) throws IllegalArgumentException,   
-	SecurityException, IllegalStateException, IOException{
-		m.reset();
-		if(m.isPlaying()){
-			m.stop();	
-		}else{
-			m.setDataSource(fileName);
-		}
-		m.prepare();
-		m.start();
-		m.seekTo(0);
-		Toast.makeText(getApplicationContext(), "Playing audio from beginning", 
-				Toast.LENGTH_LONG).show();
-
-	}
-	public void flag1(View view) throws IllegalArgumentException,   
-	SecurityException, IllegalStateException, IOException{
-		m.reset();
-		if(m.isPlaying()){
-			m.stop();	
-		}else{
-			m.setDataSource(fileName);
-		}
-		m.prepare();
-		m.start();
-		m.seekTo(FlagRelTimes.get(0));
-		Toast.makeText(getApplicationContext(), "Playing audio from flag1", 
-				Toast.LENGTH_LONG).show();
-
-	}
-	public void flag2(View view) throws IllegalArgumentException,   
-	SecurityException, IllegalStateException, IOException{
-		m.reset();
-		if(m.isPlaying()){
-			m.stop();	
-		}else{
-			m.setDataSource(fileName);
-		}
-		m.prepare();
-		m.start();
-		m.seekTo(FlagRelTimes.get(1));
-		Toast.makeText(getApplicationContext(), "Playing audio from flag2", 
-				Toast.LENGTH_LONG).show();
-
-	}
-	public void flag3(View view) throws IllegalArgumentException,   
-	SecurityException, IllegalStateException, IOException{
-		m.reset();
-		if(m.isPlaying()){
-			m.stop();	
-		}else{
-			m.setDataSource(fileName);
-		}
-		m.prepare();
-		m.start();
-		m.seekTo(FlagRelTimes.get(2));
-		Toast.makeText(getApplicationContext(), "Playing audio from flag3", 
-				Toast.LENGTH_LONG).show();
-
-	}
+	
+	
+	
+	
+	
 	
 }
